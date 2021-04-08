e3340ddc9de5a3772ebd3ce6c056661e48ec6572
Andy Park <chanhyp1@uci.edu>
2021-04-07 20:55:38 -0700

    Finish plot and read_json

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from plot import plot
 if __name__ == "__main__":
     path = "etc-00083543.json"
 
-    data = read_json(path)
-    plot(data)
-
+    guide_gmm = read_json(path)
+    plot(guide_gmm)
