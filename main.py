b55061e58978c9ecd8d724cc402a6817e8725b97
Andy Park <chanhyp1@uci.edu>
2021-04-13 16:59:55 -0700

    Add get_telemetry

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from util import get_json, read_json
+from util import get_json, read_json, get_telemetry
 from plot import plot
 
 if __name__ == "__main__":
 
-    path = "/global/cfs/cdirs/desi/spectro/data"
-    etc_files = get_json(path)
+    # path = "/global/cfs/cdirs/desi/spectro/data"
+    # etc_files = get_json(path)
 
-    for etc_file in etc_files:
-        data = read_json(etc_file)
-        plot(data)
+    # for etc_file in etc_files:
+    #     data = read_json(etc_file)
+    #     plot(data)
+
+    print(get_telemetry())
--- util.py
+++ util.py
@@ -2,6 +2,7 @@ import os
 import json
 import numpy as np
 import desietc.gmm
+import desietc.db
 
 
 def get_json(path):
@@ -59,3 +60,27 @@ def read_json(path):
             }
 
     return data
+
+def get_telemetry():
+    """
+    Query online telemetry DB for wind speed, direction, and
+    telescope position
+    """ 
+    telescope = query("""
+                SELECT DATE(T.time_recorded), AVG(T.wind_speed)
+                    AS wind_speed, AVG(T.wind_direction) AS wind_direction
+                FROM telemetry.environmentmonitor_tower AS T
+                WHERE T.time_recorded > '20210319'
+                GROUP BY DATE(T.time_recorded)
+                ORDER BY DATE(T.time_recorded) DESC
+    """, maxrows=50)
+    return telescope
+
+
+def query(sql, maxrows=10, dates=None):
+    """
+    Individual query
+    """
+    db = desietc.db.DB()
+    df = db.query(sql, maxrows, dates)
+    return df
