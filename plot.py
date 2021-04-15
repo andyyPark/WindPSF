bb7085e7224fed406f3a9ccf3c2c59da31acfabd
Andy Park <chanhyp1@uci.edu>
2021-04-14 20:59:14 -0700

    Fix vmin, vmax

--- plot.py
+++ plot.py
@@ -15,6 +15,9 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
     if len(guide_gmm.keys()) == 1:
         print(data["expinfo"])
 
+    model_sum = {guide: guide_gmm[guide]["model"].sum() for guide in guide_gmm.keys()}
+    model_max = np.median([guide_gmm[guide]["model"].max() / model_sum[guide] for guide in guide_gmm.keys()])
+    vmin, vmax = -0.1 * model_max, 1.0 * model_max
     norm = np.max(np.array([guide_gmm[guide]["model"].sum() for guide in guide_gmm.keys()]))
 
     fig, axs = plt.subplots(nrows=nrows, ncols=len(guide_gmm.keys()), figsize=figsize)
@@ -31,6 +34,7 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
 
         axs[i].imshow(
             guide_gmm[guide]["model"] / norm,
+            vmin=vmin, vmax=vmax,
             origin="lower",
             extent=2
             * [
@@ -40,7 +44,7 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
             cmap=plt.cm.magma,
             interpolation="bicubic",
         )
-        fiber = plt.Circle((0, 0), radius, color="g", fill=False)
+        fiber = plt.Circle((0, 0), radius, color="g", fill=False, alpha=0.7)
         axs[i].add_patch(fiber)
         axs[i].grid(False)
         axs[i].axis("off")
@@ -52,7 +56,10 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
         e1 = shear_data[guide]['e1']
         e2 = shear_data[guide]['e2']
 
-        draw_ellipse_se1e2(axs[i], 0, 0, s, e1, e2)
+        if data['expinfo']['night'] == '20210326' and data['expinfo']['expid'] == 82357:
+            print(s, e1, e2) 
+
+        draw_ellipse_se1e2(axs[i], 0, 0, s, e1, e2, data['expinfo'])
 
         if i == 0:
             expid = "0" * (8 - len(str(data["expinfo"]["expid"]))) + str(
@@ -66,7 +73,7 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
                 verticalalignment="bottom",
                 horizontalalignment="left",
                 transform=axs[i].transAxes,
-                color="white",
+                color="w",
                 fontsize=15,
             )
             axs[i].set_zorder(1)
@@ -82,16 +89,18 @@ def plot(data, shear_data, nrows=1, figsize=(12, 10)):
     plt.savefig(path)
     plt.close()
 
-def draw_ellipse_se1e2(ax, x0, y0, s, g1, g2, nsigmas=1, **ellipseopts):
+def draw_ellipse_se1e2(ax, x0, y0, s, g1, g2, info, nsigmas=1, **ellipseopts):
     g = np.sqrt(g1 ** 2 + g2 ** 2)
     if g > 1:
+        print(info, 'failed')
+        print(g1 ** 2 + g2 ** 2)
         raise ValueError('g1 ** 2 + g2 ** 2 > 1')
     center = np.array([x0, y0])
     angle = np.rad2deg(0.5 * np.arctan2(g2, g1))
     ratio = np.sqrt((1 + g) / (1 - g))
     width = 2 * s * ratio * nsigmas
     height  = 2 * s / ratio * nsigmas
-    kwargs = dict(color='r', ls='-', lw=2, alpha=0.7, fill=False)
+    kwargs = dict(color='w', ls='-', lw=2, alpha=0.7, fill=False)
     kwargs.update(ellipseopts)
     ellipse = matplotlib.patches.Ellipse(center, width, height, angle, **kwargs)
     ax.add_artist(ellipse)
